0ed8e8070d5d3fe7a8ec2b5b292c25a264487d41
jeeva <jeevannam06@gmail.com>
2024-09-21 22:08:00 +0530

    Adding comments and reformat

--- structy.py
+++ structy.py
@@ -1,11 +1,15 @@
 from math import sqrt, floor
 
 ''' Find max number from the list '''
+
+
 def max_value(nums):
     return max(nums)
 
 
 ''' Find given number is prime or not '''
+
+
 def is_prime(n):
     if n < 2:
         return False
