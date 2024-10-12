56a9063a7f9996685f178bf2bac9129a2cdcb660
jeevananm06 <jeevananm06@gmail.com>
2024-10-12 16:09:36 +0530

    completed

--- uncompress.py
+++ uncompress.py
@@ -1,9 +1,36 @@
 def uncompress(s):
-    j=''
-    for i in range(0, len(s), 2):
-        k = s[i:i + 2]
-        j += int(k[0])*k[1]
-    return j
+    result = []
+    i = 0
+    for i in range(0, len(s)):
+        num = 0
+        # Extract the number
+        while s[i].isdigit():
+            num = num * 10 + int(s[i])
+            i += 1
+        # Extract the character
+        char = s[i]
+        i += 1
+        # Append the character 'num' times to the result
+        result.append(char * num)
+    return ''.join(result)
 
 
-print(uncompress('2c3a1t'))
+# structy soln
+def uncompress1(s):
+    numbers = '0123456789'
+    result = []
+    i = 0
+    j = 0
+    while j < len(s):
+        if s[j] in numbers:
+            j += 1
+        else:
+            num = int(s[i:j])
+            result.append(s[j] * num)
+            j += 1
+            i = j
+
+    return ''.join(result)
+
+
+print(uncompress1('112c3a1t'))
